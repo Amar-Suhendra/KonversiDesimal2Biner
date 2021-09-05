39d28f9c3078cbda6e5c332eb1e986962b39d16f
Suhendra <75647351+Amar-Suhendra@users.noreply.github.com>
2021-09-05 22:30:21 +0700

    Add Decimal 2 Binary
    Add Decimal 2 Binary

---
+++ Konversi/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Dim Jawaban As String
+    Public Function Convert(ByVal Input As Integer) As String
+        While Input > 0
+            Jawaban &= Input Mod 2
+            Input \= 2
+        End While
+        Return StrReverse(Jawaban)
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        Dim Ch As Char = e.KeyChar
+        If Not Char.IsDigit(Ch) AndAlso Asc(Ch) <> 8 Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text = "" Then
+            Label3.Text = "Biner :"
+        ElseIf TextBox1.Text = 0 Then
+            Label3.Text = "Biner : 0"
+        Else
+            Convert(TextBox1.Text)
+            Label3.Text = "Biner : " + Jawaban
+            Jawaban = ""
+        End If
+    End Sub
+
+End Class
